c3bded2884ce661309d28583be75b8d15a445b26
rryqsh <you@example.com>
2018-11-27 19:48:19 +0800

    eee

--- src/testGitHub/TestGitCommit.java
+++ src/testGitHub/TestGitCommit.java
@@ -5,7 +5,7 @@ public class TestGitCommit {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("提交到GitHub");
+		System.out.println("提交到GitHub1111");
 		
 		System.out.println("提交上了");
 	}
